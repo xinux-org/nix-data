7892550ca6d5dc7a5d2334ce6c6109a0dd73c29b
bahrom04 <magdiyevbahrom@gmail.com>
2025-09-17 12:54:40 +0500

    chore: add direnv support and move nixpkgs to xinux-org/nixpkgs

--- shells/nix-data/default.nix
+++ shells/nix-data/default.nix
@@ -1,16 +1,17 @@
-{ mkShell
-, rust-analyzer
-, rustc
-, rustfmt
-, cargo
-, cargo-tarpaulin
-, clippy
-, openssl
-, pkg-config
-, sqlite
-, ...
+{
+  pkgs,
+  mkShell,
+  rust-analyzer,
+  rustc,
+  rustfmt,
+  cargo,
+  cargo-tarpaulin,
+  clippy,
+  openssl,
+  pkg-config,
+  sqlite,
+  ...
 }:
-
 mkShell {
   nativeBuildInputs = [
     rust-analyzer
@@ -23,4 +24,8 @@ mkShell {
     pkg-config
     sqlite
   ];
+
+  # Set Environment Variables
+  RUST_BACKTRACE = "full";
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
