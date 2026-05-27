86aef70d926ddc195d8c894bf88b640851c45049
bahrom04 <magdiyevbahrom@gmail.com>
2026-05-27 15:27:33 +0500

    chore: flake update

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   inputs = {
     nixpkgs.url = "git+https://git.oss.uzinfocom.uz/xinux/nixpkgs?ref=nixos-unstable&shallow=1";
     xinux-lib = {
-      nixpkgs.url = "git+https://git.oss.uzinfocom.uz/xinux/lib?&shallow=1";
+      url = "git+https://git.oss.uzinfocom.uz/xinux/lib?&shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs:
+  outputs =
+    inputs:
     inputs.xinux-lib.mkFlake {
       inherit inputs;
       alias.shells.default = "nix-data";
