8e3299060811f64de84f8b99dbb8ab9bbddd3b05
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-06-03 21:34:59 +0500

    fix: define oldest support for nix-data

--- modules/nixos/nix-data/default.nix
+++ modules/nixos/nix-data/default.nix
@@ -24,7 +24,7 @@ in
       ];
       visible = false;
       warn = true;
-      use = warnIf true "Obsolete option `${showOption from}' is used. It was renamed to `${showOption to}'.";
+      use = warnIf (oldestSupportedReleaseIsAtLeast "2511") "Obsolete option `${showOption from}' is used. It was renamed to `${showOption to}'.";
     })
   ];
 
